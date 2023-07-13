8d29a4b76b8f9da0d910fe86eeac36f4ed14049f
Misho <chapidze.misho95@gmail.com>
2023-07-13 19:22:16 +0400

    t

---
+++ src/components/container.tsx
@@ -0,0 +1,10 @@
+
+const Container = ({children}) => {
+    return(
+        <div className="bg-gray-200 w-full h-full min-h-screen p-2">
+            {children}
+        </div>
+    )
+}
+
+export default Container;
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return(
+        <div className="bg-green-500 p-2">Header</div>
+    )
+}
+
+export default Header;
